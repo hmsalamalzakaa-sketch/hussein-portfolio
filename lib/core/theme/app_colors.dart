d1adc884d0ee432676c917301c2f0a3b111e7cd2
hsein msalam <hmsalam.alzakaa@gmail.com>
2026-06-09 14:11:49 +0300

    feat(theme): add dark theme, colors and text styles setup

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF6C63FF);
+  static const background = Color(0xFF0F0F0F);
+  static const cardDark = Color(0xFF1C1C1C);
+  static const textLight = Colors.white;
+  static const textGrey = Colors.grey;
+}
